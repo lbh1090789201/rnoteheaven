730ad200ba2bc7cda003ee20ba0136d75baa97dc
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-05-27 14:38:20 +0800

    update

--- spec/helpers/apply_records_helper_spec.rb
+++ spec/helpers/apply_records_helper_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Webapp::ApplyRecordsHelper, type: :helper do
-  it 'test get_apply_records ' do
-    apply_record = create(:apply_record)
 
-    user = get_apply_records(1)
-    expect(apply_record.resume_status).to eq "MyString"
+  it 'test get_apply_records' do
+    apply_record = create(:apply_record)
+    apply_record2 = create(:apply_record2)
+    job = create(:job)
+    job2 = create(:job2)
+
+    res = get_apply_records(1)
+    expect(res.size).to eq 2
   end
-  # it 'test get_job_infos ' do
-  #   job = create(:job)
-  #
-  #   user = get_job_infos(1)
-  #   expect(apply_record.resume_status).to eq "MyString"
-  # end
+
 end
