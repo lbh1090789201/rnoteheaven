6a466f08213af0e5009d4388fadab3e25c9dd19c
文亚 <wenya@roadclouding.com>
2016-05-27 14:48:20 +0800

    526

--- spec/helpers/apply_records_helper_spec.rb
+++ spec/helpers/apply_records_helper_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe Webapp::ApplyRecordsHelper, type: :helper do
   it 'test get_apply_records ' do
     apply_record = create(:apply_record)
-
     user = get_apply_records(1)
     expect(apply_record.resume_status).to eq "MyString"
   end
   it 'test get_job_infos ' do
     job = create(:job)
-
     user = get_job_infos(1)
-    expect(apply_record.resume_status).to eq "MyString"
+    expect(job.name).to eq "MyString"
+  end
+  it 'test get_date ' do
+    job = create(:job)
+    user = get_job_infos(1)
+    expect(job.name).to eq "MyString"
   end
 end
