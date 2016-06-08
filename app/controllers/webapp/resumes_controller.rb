7d4ded7b3f897607895e6ce74f222014b3a8e3d6
libinghua <libinghua@roadclouding.com>
2016-06-08 19:14:16 +0800

    index/resume preview

--- app/controllers/webapp/resumes_controller.rb
+++ app/controllers/webapp/resumes_controller.rb
@@ -13,6 +13,15 @@ class Webapp::ResumesController < ApplicationController
   def destroy
   end
 
+  def preview
+    resume = Resume.where(user_id: current_user.id).first_or_create!
+
+    @user = User.find_by_id(current_user.id)
+    @work_experiences = WorkExperience.where(:user_id => @user.id)
+    @education_experiences = EducationExperience.where(:user_id => @user.id)
+    @expect_job = ExpectJob.find_by_user_id(@user.id)
+  end
+
   def show
     resume = Resume.where(user_id: current_user.id).first_or_create!
 
