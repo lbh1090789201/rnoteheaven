38ce9178976ad1c5a347deb7761f8b776bc3f2c1
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-06-16 18:38:57 +0800

    fix avatar twice

--- app/controllers/webapp/users_controller.rb
+++ app/controllers/webapp/users_controller.rb
@@ -69,7 +69,7 @@ class Webapp::UsersController < ApplicationController
       resume_maturity = Resume.get_maturity @user.id
 
       if user_params[:avatar]
-        render js: 'window.location.replace(window.location.href);', notice: '修改成功'
+        render js: 'history.go(-1);window.location.replace(window.location.href);', notice: '修改成功'
       else
         render js: 'alert( "修改失败");'
       end
