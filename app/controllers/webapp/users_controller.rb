83612064dd703966167dbc55303ff53407c86d2e
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-06-16 18:04:33 +0800

    fix upload

--- app/controllers/webapp/users_controller.rb
+++ app/controllers/webapp/users_controller.rb
@@ -69,7 +69,7 @@ class Webapp::UsersController < ApplicationController
       resume_maturity = Resume.get_maturity @user.id
 
       if user_params[:avatar]
-        render js: 'location.reload();', notice: '修改成功'
+        render js: 'refreshOnce();', notice: '修改成功'
       else
         render js: 'history.go(-1);', notice: '修改成功'
       end
