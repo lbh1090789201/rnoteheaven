0586894d4a57a6c80266ea9a2961860f3a711e4e
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-06-16 16:42:07 +0800

    use ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh uprunning@hb.yundaioa.com/ryunkang
+ssh -t uprunning@hb.yundaioa.com "cd ryunkang; git pull; rm -rf public/assets/; RAILS_ENV=production bundle exec rake assets:precompile; sudo service nginx stop; sudo killall nginx; sudo service nginx start; bash;"
