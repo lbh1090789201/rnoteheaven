23974d0629b3662570e810672cf9f29e5c7f9f27
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-06-16 16:48:24 +0800

    fix ssh

--- ssh.sh
+++ ssh.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh -t uprunning@hb.yundaioa.com "cd ryunkang; git pull; rm -rf public/assets/; RAILS_ENV=production bundle exec rake assets:precompile; sudo service nginx stop; sudo killall nginx; sudo service nginx start;"
+ssh -t uprunning@hb.yundaioa.com "cd ryunkang"
