101b16b8758b906d62d4890c52732c6ab9fbef51
libinghua <libinghua@roadclouding.com>
2016-06-27 15:04:30 +0800

    job new

--- app/assets/javascripts/employer.js
+++ app/assets/javascripts/employer.js
@@ -19,14 +19,45 @@ function JobBasicNew(obj,api,pclass) {
   var span = document.createElement("span");
   span.setAttribute('class','title-over');
   span.setAttribute('onclick','TitleOver(this)');
-  console.log(span);
   var div = $(".top");
   div.prepend(span);
   $(document).scrollTop('0');
 }
 
 function change_val(obj) {
-  $("#" + $(obj).attr("class")).attr('value', obj.value);
+  var input = $("#" + $(obj).attr("class"));
+  if(input.parent().attr('class') == 'job-endtime'){
+    input.text(obj.value);
+    var val = obj.value;
+    switch(val){
+      case '１星期':
+        val = 7;
+        input.siblings().children('input').attr('value', val);
+        break;
+      case '２星期':
+        val = 14;
+        input.siblings().children('input').attr('value', val);
+        break;
+      case '半个月':
+        val = 15;
+        input.siblings().children('input').attr('value', val);
+        break;
+      case '１个月':
+        val = 31;
+        input.siblings().children('input').attr('value', val);
+        break;
+      case '２个月':
+        val = 62;
+        input.siblings().children('input').attr('value', val);
+        break;
+      case '３个月':
+        val = 93;
+        input.siblings().children('input').attr('value', val);
+        break;
+    }
+  }else {
+    input.attr('value', obj.value);
+  }
   $('.title').text(edit_title);
   $('.before-mask').animate({
     top: '1500px',
