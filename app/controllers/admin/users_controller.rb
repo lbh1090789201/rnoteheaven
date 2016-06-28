5e9e8b4abfc4708cd4a1a161e42c299638ca54a9
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-06-28 12:25:28 +0800

    finish admin-user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,16 @@ class Admin::UsersController < ApplicationController
   layout 'admin'
 
   def index
+    if params[:filter]
+      @users = User.filter_user_status('reviewing')
+                 .filter_release_before(params[:time_before])
+                 .filter_release_before(params[:time_after])
+                 .filter_user_type(params[:user_type])
+                 .filter_hospital_name(params[:hospital_name])
+                 .filter_user_name(params[:user_name])
+    else
+      @users = User.all
+    end
   end
 
   def edit
