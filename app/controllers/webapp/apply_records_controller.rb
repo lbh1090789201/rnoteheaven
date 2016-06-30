5da1719039dcba4218d0686d61c7b71308033163
libinghua <libinghua@roadclouding.com>
2016-06-30 09:54:22 +0800

    fix resume-view

--- app/controllers/webapp/apply_records_controller.rb
+++ app/controllers/webapp/apply_records_controller.rb
@@ -9,22 +9,23 @@ class Webapp::ApplyRecordsController < ApplicationController
   end
 
   def show
-    @apply_record = ApplyRecord.find_by(params[:id])
-    @job = Job.find_by @apply_record.job_id
-    @hospital = Hospital.find_by @job.hospital_id
-    @applyrecord = []
-    o = {
-      id: @apply_record.job_id,
-      job_name: @job.name,
-      salary_range: @job.salary_range,
-      created_at: @apply_record.created_at,
-      hospital_name: @hospital.name,
-      resume_status: @apply_record.resume_status,
-      apply_at: @apply_record.apply_at,
-      view_at: @apply_record.view_at,
-      recieve_at: @apply_record.recieve_at,
-      location: @job.location
-    }
-    @applyrecord.push(o)
+    @apply_record = ApplyRecord.find params[:id]
+    # @job = Job.find_by @apply_record.job_id
+    @hospital = Hospital.find_by @apply_record.hospital_id
+    # @applyrecord = []
+    # o = {
+    #   id: @apply_record.job_id,
+    #   job_name: @job.name,
+    #   salary_range: @job.salary_range,
+    #   created_at: @apply_record.created_at,
+    #   hospital_name: @hospital.name,
+    #   resume_status: @apply_record.resume_status,
+    #   apply_at: @apply_record.apply_at,
+    #   view_at: @apply_record.view_at,
+    #   recieve_at: @apply_record.recieve_at,
+    #   location: @job.location
+    # }
+    # @applyrecord.push(o)
+    puts "----------"+@applyrecord.to_json.to_s
   end
 end
