db502fde0637a40f033a05f1e257ffaaaf19c4c2
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-07-07 13:34:31 +0800

    fix slice

--- app/assets/javascripts/components/admin_jobs/job.js.jsx
+++ app/assets/javascripts/components/admin_jobs/job.js.jsx
@@ -7,7 +7,7 @@ var Job = React.createClass({
         <td>{this.props.data.job_type}</td>
         <td>行业</td>
         <td>发布机构</td>
-        <td>{this.props.data.submit_at.slice(0,10)}</td>
+        <td>{this.props.data.submit_at ? this.props.data.submit_at.slice(0,10) : '----'}</td>
         <td>{this.props.data.status}</td>
         <td>查看</td>
         <td><input onChange={this.props.handleCheck} type="checkBox" value={this.props.data.id} /></td>
