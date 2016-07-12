bc600407910b8d0c1c87e2ec2b67e6e78c890ca8
libinghua <libinghua@roadclouding.com>
2016-07-12 15:51:59 +0800

    fix resume css

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  # before :each do
-  #
-  # end
+  before :each do
+   @hospital = create(:hospital)
+  end
 end
