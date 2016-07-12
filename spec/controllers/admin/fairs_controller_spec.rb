8fc2bc80302eebf24214b10d2e9b888df377fe2d
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-07-12 11:56:52 +0800

    fix all respec

--- spec/controllers/admin/fairs_controller_spec.rb
+++ spec/controllers/admin/fairs_controller_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Admin::FairsController, type: :controller do
+  render_views
+  let(:json) { JSON.parse(response.body) }
 
-  describe "GET #index" do
-    it "returns http success" do
+    before :each do
+      @user = create(:user)
+      @user.add_role :admin
+      login_with @user
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+  describe "test pages connect" do
+    it "GET#index returns http success" do
       get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET#history returns http success" do
+      get :history
       expect(response).to have_http_status(:success)
     end
   end
--- spec/controllers/admin/hospitals_controller_spec.rb
+++ spec/controllers/admin/hospitals_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Admin::HospitalsController, type: :controller do
+  render_views
+  let(:json) { JSON.parse(response.body) }
+
+    before :each do
+      @user = create(:user)
+      @user.add_role :admin
+      login_with @user
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
 
   describe "GET #index" do
     it "returns http success" do
