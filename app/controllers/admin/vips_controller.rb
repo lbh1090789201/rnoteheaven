00d2484acff6ded4f8c8f35f4e832669a4a5496b
libinghua <libinghua@roadclouding.com>
2016-07-12 14:53:18 +0800

    fix admin resume css

--- app/controllers/admin/vips_controller.rb
+++ app/controllers/admin/vips_controller.rb
@@ -1,5 +1,6 @@
 class Admin::VipsController < AdminController
   before_action :require_vips_manager!
+    # set_vip = Employer.set_vip current_user.id, 1
 
   def index
   end
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
-  factory :plan do
-    
+  factory :plan, :class => 'plan' do
+    name 'vip1'
+    may_recieve "10"
+    may_release "10"
+    may_set_top "10"
+    may_view "10"
+    may_join_fairs "10"
+    status true
   end
 end
