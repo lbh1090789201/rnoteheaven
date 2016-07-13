1fc25f846ca6f1c95ffc9a2dbb41e14ef984caf2
libinghua <libinghua@roadclouding.com>
2016-07-13 09:56:02 +0800

    fix vip

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,12 @@
 class Plan < ActiveRecord::Base
+
+  #按套餐名称查找
+  scope :filter_by_name, ->(name) {
+    where('name LIKE ?', "#{name}") if name.present?
+  }
+
+  #按状态查找
+  scope :filter_by_status, ->(status) {
+    where(status: status) if status.present?
+  }
 end
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
-  factory :plan, :class => 'plan' do
+  factory :plan, :class => 'Plan' do
     name 'vip1'
-    may_recieve "10"
-    may_release "10"
-    may_set_top "10"
-    may_view "10"
-    may_join_fairs "10"
+    may_receive 10
+    may_release 10
+    may_set_top 10
+    may_view 10
+    may_join_fairs 10
     status true
   end
 end
