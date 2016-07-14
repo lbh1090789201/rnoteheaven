e895b47e92bff4cbe807bb57f639c28f70809acc
luyunstaff020 <zhangwenbo@gitlab.roadclouding.com>
2016-07-14 18:48:00 +0800

    fix select

--- app/assets/javascripts/components/admin_hospitals/hospital_edit.js.jsx
+++ app/assets/javascripts/components/admin_hospitals/hospital_edit.js.jsx
@@ -1,7 +1,7 @@
 var AdminEditHospital = React.createClass({
   getInitialState: function() {
     return {
-      vip_id: 1,
+      vip_id: this.props.data.vip_id,
       vip_name: this.props.data.vip_name,
       plan: '',
       index: this.props.dad.state.hos_info.index,
@@ -59,7 +59,7 @@ var AdminEditHospital = React.createClass({
         select_plan = plans.map(
           function(plan, index) {
             return (
-              <AdminHosEditPlanItem key={plan.id} data={plan} dad={this.props.dad} />
+              <option key={plan.id} value={plan.id}>{plan.name}</option>
             )
           }.bind(this)
         )
@@ -68,104 +68,92 @@ var AdminEditHospital = React.createClass({
       <div className="mask-user">
         <div className="user-box">
           <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-               <label>申请机构</label>
+            <div className="form-group col-sm-4">
+               <label>账号</label>
+                 <input type="text" className="form-control" placeholder="账号" name="hospital_id"
+                        disabled  required ref="id" defaultValue={this.props.data.contact_number} />
+            </div>
+
+            <div className="form-group col-sm-4">
+               <label>机构名称</label>
                  <input className="form-control" type="text" placeholder="机构名称"
                     name="name" required ref="hospital_name" defaultValue={this.props.data.hospital_name} />
             </div>
 
-            <div className="form-group">
+            <div className="form-group col-sm-4">
+               <label>负责人</label>
+                 <input type="text" className="form-control" placeholder="姓名" name="contact_person"
+                          required ref="contact_person" defaultValue={this.props.data.contact_person} />
+            </div>
+
+            <div className="form-group col-sm-4">
+               <label>联系电话</label>
+                 <input type="text" className="form-control" placeholder="手机号码" name="contact_number"
+                          required ref="contact_number" defaultValue={this.props.data.contact_number} />
+            </div>
+
+            <div className="form-group col-sm-4">
                <label>行业</label>
                  <input type="text" className="form-control" placeholder="行业" name="industry"
                                required ref="industry" defaultValue={this.props.data.industry} />
             </div>
 
-            <div className="form-group">
+            <div className="form-group col-sm-4">
                <label>性质</label>
                  <input type="text" className="form-control" placeholder="性质" name="property"
                                required ref="property" defaultValue={this.props.data.property} />
             </div>
 
-            <div className="form-group">
+            <div className="form-group col-sm-4">
                <label>规模</label>
                  <input type="text" className="form-control" placeholder="规模" name="scale"
                             required ref="scale" defaultValue={this.props.data.scale} />
             </div>
 
-            <div className="form-group">
+            <div className="form-group col-sm-4">
                <label>地区</label>
                  <input type="text" className="form-control" placeholder="地区" name="region"
                            required ref="region" defaultValue={this.props.data.region} />
             </div>
 
-            <div className="form-group">
+            <div className="form-group col-sm-4">
+               <label>级别配置</label>(原：{this.props.data.vip_name})
+               <select onChange={this.handleSelect} name="vip_name" defaultValue={this.props.data.vip_id} className="form-control" >
+                 {select_plan}
+               </select>
+            </div>
+
+            <div className="form-group col-sm-12">
                <label>机构地址</label>
                  <input type="text" className="form-control" placeholder="机构地址" name="location"
                            required ref="location" defaultValue={this.props.data.location} />
             </div>
 
-            <div className="form-group">
-               <label>账号</label>
-                 <input type="text" className="form-control" placeholder="账号" name="hospital_id"
-                          required ref="id" defaultValue={this.props.data.id} />
-            </div>
-
-            <div className="form-group">
-               <label>姓名</label>
-                 <input type="text" className="form-control" placeholder="姓名" name="contact_person"
-                          required ref="contact_person" defaultValue={this.props.data.contact_person} />
-            </div>
-
-            <div className="form-group">
-               <label>手机号码</label>
-                 <input type="text" className="form-control" placeholder="手机号码" name="contact_number"
-                          required ref="contact_number" defaultValue={this.props.data.contact_number} />
-            </div>
-
-            <div className="form-group">
+            <div className="form-group col-sm-12">
                <label>机构介绍</label>
-                 <input type="text" className="form-control" placeholder="机构介绍" name="introduction"
-                          required ref="introduction" defaultValue={this.props.data.introduction} />
+                 <textarea type="text" className="form-control" placeholder="机构介绍" name="introduction" rows="5"
+                          pattern=".{6,}" required title="最少6个字符" ref="introduction" defaultValue={this.props.data.introduction} />
             </div>
 
-            <div className="form-group">
-               <label>级别配置(当前级别：{this.props.data.vip_name})</label>
-               <select onChange={this.handleSelect} className="form-control" >
-                 {select_plan}
-               </select>
-            </div>
 
-            <div className="form-group">
-              <div>{this.state.plan.name}:
-                 <span>可发布职位{this.state.plan.may_release}个</span>
-                 <span>可置顶职位{this.state.plan.may_set_top}个</span>
-                 <span>可接收简历{this.state.plan.may_receive}份</span>
-                 <span>可查看简历{this.state.plan.may_view}份</span>
-                 <span>可参加专场{this.state.plan.may_join_fairs}次</span>
-              </div>
-            </div>
+            {
+              // <div className="form-group">
+              //   <div>{this.state.plan.name}:
+              //      <span>可发布职位{this.state.plan.may_release}个</span>
+              //      <span>可置顶职位{this.state.plan.may_set_top}个</span>
+              //      <span>可接收简历{this.state.plan.may_receive}份</span>
+              //      <span>可查看简历{this.state.plan.may_view}份</span>
+              //      <span>可参加专场{this.state.plan.may_join_fairs}次</span>
+              //   </div>
+              // </div>
+            }
 
-            <button type="button" className="btn btn-secondary" onClick={this.handleClick}>取消</button>
-            <button type="submit" className="btn btn-success">提交</button>
+
+            <button type="button" className="btn btn-secondary btn-bottom" onClick={this.handleClick}>取消</button>
+            <button type="submit" className="btn btn-success btn-bottom">提交</button>
           </form>
         </div>
       </div>
-    )
-  }
-})
-
-
-/*************下拉框组件***************/
-var AdminHosEditPlanItem = React.createClass({
-  handleClick: function(e) {
-    e.preventDefault()
-    this.props.dad.setState({
-      plan: this.props.data,
-    })
-  }
-  ,render: function() {
-    return (
-      <option onMouseOver={this.handleClick} value={this.props.data.id} name="vip_name" ref="plan_info">{this.props.data.name}</option>
     )
   }
 })
