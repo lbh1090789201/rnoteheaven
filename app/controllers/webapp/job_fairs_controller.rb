658da7e9b057c6fb9670075c0f2d4fc47329bfc2
libinghua <libinghua@roadclouding.com>
2016-07-15 12:36:49 +0800

    webapp job_fair

--- app/controllers/webapp/job_fairs_controller.rb
+++ app/controllers/webapp/job_fairs_controller.rb
@@ -1,7 +1,36 @@
 class Webapp::JobFairsController < ApplicationController
   before_action :authenticate_user!   # 登陆验证
 
+  def index
+    @fairs = Fair.where(status: 'processing')
+    # @fairs = []
+    # fairs.each do |f|
+    #   o = {
+    #     fair = f,
+    #     left_end = ((f.end_at - Time.now)/1.day).to_i
+    #   }
+    #   @fairs.push(o)
+    # end
+    # return @fairs
+    @fairs_length = @fairs.length
+  end
+
   def show
+    fair = Fair.find params[:id]
+    fair_hospitals = FairHospital.where(fair_id: fair.id, status: 'on')
+
+    @fair_hospitals = []
+
+    fair_hospitals.each do |f|
+      hospital = Hospital.find f.hospital_id
+      o = {
+        banner: f.banner,
+        hospital: hospital,
+      }
+      @fair_hospitals.push o
+    end
+    return @fair_hospitals
+
   end
 
   def edit
@@ -15,5 +44,5 @@ class Webapp::JobFairsController < ApplicationController
 
   def destroy
   end
-  
+
 end
