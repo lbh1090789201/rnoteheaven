94ebf71c8c1ed69c74e0419a7d290c70b0301e75
zhangwenbo <zhangwenbo@gitlab.roadclouding.com>
2016-07-20 10:44:59 +0800

    screen

--- app/assets/javascripts/yunkang.js
+++ app/assets/javascripts/yunkang.js
@@ -27,12 +27,23 @@ $(document).scrollTop('0');
 
 /*屏幕自适应开始*/
 // var screenWidth = window.screen.width;
-var screenWidth = (ionic.Platform.isIOS()) ? window.screen.width : window.innerWidth * window.devicePixelRatio;
+var screenRatio;
+var width = screen.height;
+var height = screen.width;
+var realWidth;
+var realHeight;
 
-if (screenWidth == 414){
+if(isNaN(screenRatio)){
+if(window.innerHeight>window.innerWidth)
+{realWidth=window.innerHeight; realHeight= window.innerWidth ;screenRatio = (window.innerWidth/window.innerHeight);}
+ else
+ {realWidth=window.innerWidth; realHeight= window.innerHeight;screenRatio = (window.innerHeight/window.innerWidth);}
+
+}
+if (realWidth == 414){
     $('html').css('font-size','20px');
 }else{
-    var font_size = Math.round(screenWidth/414*20) + 'px';
+    var font_size = Math.round(realWidth/414*20) + 'px';
     $('html').css('fontSize', font_size);
     console.log(font_size)
 };
