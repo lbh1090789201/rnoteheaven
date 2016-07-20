5844c53fafb5da8af4e41d721dcd03ef741b4266
zhangwenbo <zhangwenbo@gitlab.roadclouding.com>
2016-07-20 14:45:53 +0800

    fix admin login

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -4,6 +4,7 @@ class Devise::SessionsController < DeviseController
   prepend_before_action :verify_signed_out_user, only: :destroy
   prepend_before_action only: [:create, :destroy] { request.env["devise.skip_timeout"] = true }
 
+
   # GET /resource/sign_in
   def new
         self.resource = resource_class.new(sign_in_params)
