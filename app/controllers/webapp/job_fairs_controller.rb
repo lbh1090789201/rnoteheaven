72c38ba13d551e8c87ecb9de4aa40b219cc2ee88
zhangwenbo <zhangwenbo@gitlab.roadclouding.com>
2016-07-22 18:56:33 +0800

    fix time

--- app/controllers/webapp/job_fairs_controller.rb
+++ app/controllers/webapp/job_fairs_controller.rb
@@ -2,16 +2,11 @@ class Webapp::JobFairsController < ApplicationController
   before_action :authenticate_user!   # 登陆验证
 
   def index
-    @fairs = Fair.where(status: 'processing')
-    # @fairs = []
-    # fairs.each do |f|
-    #   o = {
-    #     fair = f,
-    #     left_end = ((f.end_at - Time.now)/1.day).to_i
-    #   }
-    #   @fairs.push(o)
-    # end
-    # return @fairs
+    fairs = Fair.where(status: 'processing')
+    fairs.each do |f|
+      f.end_at = time_diff Time.now, f.end_at
+    end
+    @fairs = fairs
     @fairs_length = @fairs.length
   end
 
@@ -46,6 +41,21 @@ class Webapp::JobFairsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def time_diff(start_time, end_time)
+    seconds_diff = (start_time - end_time).to_i.abs
+
+    hours = seconds_diff / 3600
+    seconds_diff -= hours * 3600
+
+    minutes = seconds_diff / 60
+    seconds_diff -= minutes * 60
+
+    seconds = seconds_diff
+
+    "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
   end
 
 end
