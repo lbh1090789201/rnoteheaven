9d6d9ee7f3c6a49961a8d4a966b0c640de9a64f1
zhangwenbo <zhangwenbo@gitlab.roadclouding.com>
2016-08-06 09:28:07 +0800

    add validate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,15 +2,21 @@
 //= require jquery_ujs
 //= require jquery.remotipart
 //= require jquery.form
+//= require messages_zh.min
+//= require jquery.validate.min
+
 //= require bootstrap
 //= require bootstrap/alert
+
 //= require cityData
 //= require cityPicker
 //= require cityPickerEdit
-//= require yunkang
+
 //= require react
 //= require react_ujs
 //= require components
+
 //= require iscroll
 
+//= require yunkang
 //= require yunkang_fix
