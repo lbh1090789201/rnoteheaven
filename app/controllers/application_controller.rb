bedff981be789c4e9adcf1f5969198f501d790b1
zhangwenbo <zhangwenbo@gitlab.roadclouding.com>
2016-08-09 15:34:59 +0800

    to fix home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,20 +36,6 @@ class ApplicationController < ActionController::Base
   end
 
 
-
-  # Auto-sign out locked users
-  # def reject_locked!
-  #   if current_user && current_user.locked?
-  #     sign_out current_user
-  #     user_session = nil
-  #     current_user = nil
-  #     flash[:alert] = "Your account is locked."
-  #     flash[:notice] = nil
-  #     redirect_to root_url
-  #   end
-  # end
-  # helper_method :reject_locked!
-
   # Only permits admin users
   helper_method :require_admin!
   def require_admin!
