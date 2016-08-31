95e0dcad7e304f22f4f3b3f17221d4a98db5156c
libinghua <libinghua@roadclouding.com>
2016-08-31 14:31:31 +0800

    fix 定位

--- app/controllers/api/connect_app_controller.rb
+++ app/controllers/api/connect_app_controller.rb
@@ -77,7 +77,7 @@ class Api::ConnectAppController < ApiController
 
 
       logger.info('token_test:' + @user_info.to_json.to_s)
-      logger.info('token_test:' + params[:lat]+'----'+params[:lng])
+      logger.info('token_test:' + params[:lat].to_s+'----'+params[:lng].to_s)
       p "222222222222222"
       if @user_info["responseCode"] == "200"
         auto_login @user_info["userInfo"]
